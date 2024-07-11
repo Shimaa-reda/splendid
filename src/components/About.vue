ee7c4c536995e33a526b136050d6405ed784467c
Shimaa Reda <81780715+Shimaa-reda@users.noreply.github.com>
2024-07-11 21:40:49 +0300

    updates

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,29 +2,29 @@
   <div class="container ">
     <div class="row mt-5 d-flex flex-row justify-content-center align-items-center ">
         <div class="col-sm-12 col-md-6 col-lg-6">
-            <img src="../assets/about1.png" alt="">
+            <img src="../assets/about1.png" alt="" class="w-100">
         </div>
-        <div class="col-sm-12 col-md-6 col-lg-6">
+        <div class="col-sm-12 col-md-6 col-lg-6 ">
             <h2 class="text-start mb-3 headText mt-5">Who we are?</h2>
-            <p>A savvy, well-seasoned advertising and marketing consultation house operating across the Middle East, dedicated to innovating strategies, coordinating, and implementing unique campaigns.</p>
-            <p>We aim to enhance patients' healthcare experiences through awareness, educational initiatives, support programs, and personalized medications.</p>
+            <p class="text-start ">A savvy, well-seasoned advertising and marketing consultation house operating across the Middle East, dedicated to innovating strategies, coordinating, and implementing unique campaigns.</p>
+            <p class="text-start ">We aim to enhance patients' healthcare experiences through awareness, educational initiatives, support programs, and personalized medications.</p>
         </div>
     </div>
 
     <div class="row mt-5 d-flex flex-row justify-content-center align-items-center">
     <div class="col-sm-12 col-md-6 col-lg-6 order-md-2">
-        <img src="../assets/about2.png" alt="">
+        <img src="../assets/about2.png" alt="" class="w-100">
     </div>
     <div class="col-sm-12 col-md-6 col-lg-6 order-md-1">
         <h2 class="text-start mb-3 headText mt-5">How we do it?</h2>
-        <p>We use strategic thinking and insight to create engaging stories and ideas. We communicate with consumers in their preferred language to impact their behavior effectively. Our specialized team, with diverse visions and backgrounds, works together to create magic.</p>
+        <p class="text-start ">We use strategic thinking and insight to create engaging stories and ideas. We communicate with consumers in their preferred language to impact their behavior effectively. Our specialized team, with diverse visions and backgrounds, works together to create magic.</p>
     </div>
 </div>
 
 
      <div class="row mt-5">
         <div class="col-sm-12 col-md-6 col-lg-5">
-            <img src="../assets/about3.png" alt="">
+            <img src="../assets/about3.png" alt="" class="w-100">
         </div>
         <div class="col-sm-12 col-md-6 col-lg-7">
             <h2 class="text-start mb-3 headText mt-5">Mission & Vision</h2>
@@ -35,11 +35,11 @@
                 </div>
                 <div class="col-10">
                      <ul>
-                <li>
+                <li class="text-start ">
                     For clients: We deliver innovative and results-driven marketing, advertising, and PR campaigns to boost awareness, sales, and growth, enhancing patient care.
 
                 </li>
-                <li>
+                <li class="text-start ">
                     For employees: We foster a healthy, exciting work atmosphere where individuals can develop ideas, educate, and fully utilize their talents.
                 </li>
             </ul>
@@ -53,7 +53,7 @@
                 </div>
                 <div class="col-10">
                  <ul style="list-style-type:none;">
-                <li>
+                <li class="text-start ">
                     We strive to overcome challenges and create magic, never stopping.
                 </li>
                 
@@ -68,12 +68,12 @@
        
         
         <div class="col-sm-12 col-md-6 col-lg-6 order-md-2">
-            <img src="../assets/about4.png" alt="">
+            <img src="../assets/about4.png" alt="" class="w-100">
         </div>
 
         <div class="col-sm-12 col-md-6 col-lg-6 order-md-1">
             <h2 class="text-start mb-3 headText mt-5">Our core & value drivers</h2>
-            <p>We put clients first, delivering top-notch creative solutions with our vast healthcare experience. Driven by passion, we balance fun with serious results and welcome all ideas. We produce smart, clever work focused on campaign objectives and recommend cost-effective tactics to achieve your goals.</p>
+            <p class="text-start ">We put clients first, delivering top-notch creative solutions with our vast healthcare experience. Driven by passion, we balance fun with serious results and welcome all ideas. We produce smart, clever work focused on campaign objectives and recommend cost-effective tactics to achieve your goals.</p>
         </div>
 
     </div>
